57ffbb05dd234166f57dd071aa7923959acf730c
Mark Rebec <mark@markrebec.com>
2019-03-03 14:43:46 -0800

    cleans up helper and proxies a bit and implements run/render

--- lib/trail_guide/helper.rb
+++ lib/trail_guide/helper.rb
@@ -1,50 +1,73 @@
 module TrailGuide
   module Helper
     def trailguide(metric=nil, **opts, &block)
+      @trailguide_proxy ||= HelperProxy.new(self)
       if metric.nil?
-        HelperProxy.new(self)
+        @trailguide_proxy
       else
-        MetricProxy.new(self, metric).choose!(**opts, &block)
+        @trailguide_proxy.choose!(metric, **opts, &block)
       end
     end
 
     class HelperProxy
       attr_reader :context
 
-      def initialize(context)
+      def initialize(context, participant: nil)
         @context = context
+        @participant = participant
       end
 
       def new(metric)
-        MetricProxy.new(context, metric)
+        MetricProxy.new(context, metric, participant: participant)
       end
 
       def choose!(metric, **opts, &block)
         new(metric).choose!(**opts, &block)
       end
 
-      def run!(metric)
-        new(metric).run!
+      def run!(metric, **opts)
+        new(metric).run!(**opts)
       end
 
-      def render!(metric)
-        new(metric).render!
+      def render!(metric, **opts)
+        new(metric).render!(**opts)
       end
 
       def convert!(metric, checkpoint=nil, &block)
         new(metric).convert!(checkpoint, &block)
       end
+
+      def participant
+        # TODO rework participant adapters to accept a user instead of a context
+        @participant ||= begin
+          if context.respond_to?(:trailguide_participant, true)
+            TrailGuide::Participant.new(Struct.new(:current_user).new(context.send(:trailguide_participant)))
+          elsif context.respond_to?(:current_user, true)
+            TrailGuide::Participant.new(context)
+          else
+            # TODO temporary while devloping/testing
+            TrailGuide::Participant.new(Struct.new(:current_user).new(Struct.new(:id).new(rand(1...9999))))
+          end
+        end
+      end
+
+      def context_type
+        if context.is_a?(ActionView::Context)
+          :template
+        elsif context.is_a?(ActionController::Base)
+          :controller
+        end
+      end
     end
 
-    class MetricProxy
-      attr_reader :context, :metric
+    class MetricProxy < HelperProxy
+      attr_reader :metric
 
-      def initialize(context, metric)
-        @context = context
+      def initialize(context, metric, **opts)
+        super(context, **opts)
         @metric = metric
       end
 
-      # requires a single experiment
       def choose!(**opts, &block)
         raise ArgumentError, "Please provide a single experiment" unless experiments.length == 1
         variant = experiment.choose!(**opts) # TODO override: variant
@@ -55,25 +78,51 @@ module TrailGuide
         end
       end
 
-      # requires a single experiment
-      def run!
+      def run!(methods: nil, **opts)
         raise ArgumentError, "Please provide a single experiment" unless experiments.length == 1
-        choose! do |variant|
-          puts "RUN METHODS"
-          # TODO run methods
+        choose!(**opts) do |variant|
+          varmeth = methods[variant.name] if methods
+          varmeth ||= variant.name
+
+          unless context.respond_to?(varmeth, true)
+            if context_type == :controller
+              raise NoMethodError,
+                "You must define a controller method that matches variant `#{variant.name}` in your experiment `#{metric}`. In this case it looks like you need to define #{context.class.name}##{varmeth}(metadata={})"
+            elsif context_type == :template
+              raise NoMethodError,
+                "You must define a helper method that matches variant `#{variant.name}` in your experiment `#{metric}`. In this case it looks like you need to define ApplicationHelper##{varmeth}(metadata={})"
+            else
+              raise NoMethodError,
+                "You must define a method that matches variant `#{variant.name}` in your experiment `#{metric}`. In this case it looks like you need to define #{context.class.name}##{varmeth}(metadata={})"
+            end
+          end
+
+          arguments = context.method(varmeth).parameters
+          if arguments.empty?
+            context.send(varmeth)
+          elsif arguments.length > 1 || arguments[0][0] == :rest
+            context.send(varmeth, variant, **variant.metadata)
+          elsif arguments.length == 1
+            context.send(varmeth, **variant.metadata)
+          end
         end
       end
 
-      # requires a single experiment
-      def render!
+      def render!(prefix: nil, templates: nil, **opts)
+        raise NoMethodError, "The current context does not support rendering. Rendering is only available for controllers and views." unless context.respond_to?(:render, true)
         raise ArgumentError, "Please provide a single experiment" unless experiments.length == 1
-        choose! do |variant|
-          puts "RENDER TEMPLATES"
-          # TODO render templates
+        choose!(**opts) do |variant|
+          locals = { variant: variant, metadata: variant.metadata }
+          locals = { locals: locals } if context_type == :controller
+
+          template = templates[variant.name] if templates
+          prefix ||= (context.try(:view_context) || context).lookup_context.prefixes.first + '/'
+          template ||= "#{prefix.to_s}#{metric_descriptor.to_s.underscore}/#{variant.name.to_s.underscore}"
+
+          context.send(:render, template.to_s, **locals)
         end
       end
 
-      # can use a metric for multiple experiments
       def convert!(checkpoint=nil, &block)
         checkpoints = experiments.map { |experiment| experiment.convert!(checkpoint) }
         return false unless checkpoints.any?
@@ -92,20 +141,6 @@ module TrailGuide
 
       def experiment
         @experiment ||= experiments.first
-      end
-
-      def participant
-        # TODO rework participant adapters to accept a user instead of a context
-        @participant ||= begin
-          if context.respond_to?(:trailguide_participant, true)
-            TrailGuide::Participant.new(Struct.new(:current_user).new(context.send(:trailguide_participant)))
-          elsif context.respond_to?(:current_user, true)
-            TrailGuide::Participant.new(context)
-          else
-            # TODO temporary while devloping/testing
-            TrailGuide::Participant.new(Struct.new(:current_user).new(Struct.new(:id).new(rand(1...9999))))
-          end
-        end
       end
     end
   end
