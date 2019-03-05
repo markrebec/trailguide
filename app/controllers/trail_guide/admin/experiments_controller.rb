0041b6a8e884169766cf35cfbfcf01e24c72e724
Mark Rebec <mark@markrebec.com>
2019-03-04 22:22:20 -0800

    basic admin functionality

--- app/controllers/trail_guide/admin/experiments_controller.rb
+++ app/controllers/trail_guide/admin/experiments_controller.rb
@@ -1,7 +1,42 @@
 module TrailGuide
   module Admin
     class ExperimentsController < ApplicationController
+      before_action except: [:index] do
+        (redirect_to :back rescue redirect_to trail_guide_admin.experiments_path) and return unless experiment.present?
+      end
+
       def index
+      end
+
+      def start
+        experiment.start!
+        redirect_to :back rescue redirect_to trail_guide_admin.experiments_path
+      end
+
+      def stop
+        experiment.stop!
+        redirect_to :back rescue redirect_to trail_guide_admin.experiments_path
+      end
+
+      def reset
+        experiment.reset!
+        redirect_to :back rescue redirect_to trail_guide_admin.experiments_path
+      end
+
+      def restart
+        experiment.reset! && experiment.start!
+        redirect_to :back rescue redirect_to trail_guide_admin.experiments_path
+      end
+
+      def winner
+        experiment.declare_winner!(params[:winner])
+        redirect_to :back rescue redirect_to trail_guide_admin.experiments_path
+      end
+
+      private
+
+      def experiment
+        @experiment ||= TrailGuide.catalog.find(params[:id])
       end
     end
   end
--- config/admin/routes.rb
+++ config/admin/routes.rb
@@ -1,3 +1,11 @@
 TrailGuide::Admin::Engine.routes.draw do
-  resources :experiments, only: [:index]
+  resources :experiments, path: '/', only: [:index] do
+    member do
+      match :start,   via: [:put, :post]
+      match :stop,    via: [:put, :post]
+      match :reset,   via: [:put, :post]
+      match :restart, via: [:put, :post]
+      match 'winner/:winner', action: :winner, as: :winner, via: [:put, :post]
+    end
+  end
 end
