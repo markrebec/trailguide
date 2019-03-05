bc2846709f111fa047f0bc30032cda155a4ce4ae
Mark Rebec <mark@markrebec.com>
2019-03-04 23:34:21 -0800

    more fixes and placeholders for admin

--- config/admin/routes.rb
+++ config/admin/routes.rb
@@ -1,11 +1,11 @@
 TrailGuide::Admin::Engine.routes.draw do
   resources :experiments, path: '/', only: [:index] do
     member do
-      match :start,   via: [:put, :post]
-      match :stop,    via: [:put, :post]
-      match :reset,   via: [:put, :post]
-      match :restart, via: [:put, :post]
-      match 'winner/:winner', action: :winner, as: :winner, via: [:put, :post]
+      match :start,   via: [:put, :post, :get]
+      match :stop,    via: [:put, :post, :get]
+      match :reset,   via: [:put, :post, :get]
+      match :restart, via: [:put, :post, :get]
+      match 'winner/:winner', action: :winner, as: :winner, via: [:put, :post, :get]
     end
   end
 end
