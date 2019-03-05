dd4cd45f41adf43651d848d69fdb6d5301bcece4
Mark Rebec <mark@markrebec.com>
2019-03-04 17:40:54 -0800

    mounts engines in dummy app

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,5 +1,9 @@
+require "trail_guide/admin"
+
 Rails.application.routes.draw do
+
   mount TrailGuide::Engine => "/trailguide"
+  mount TrailGuide::Admin::Engine => "/admin/trailguide"
 
   root to: 'homepage#index'
 end
