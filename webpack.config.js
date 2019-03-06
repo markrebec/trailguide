ccf8fe44b03ffafd3650d65e85fd1685c9516792
Mark Rebec <mark@markrebec.com>
2019-03-05 22:20:39 -0800

    starts working out javascript client

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path')
+
+// TODO create another config to export for modern browsers
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'app/assets/javascripts/'),
+    filename: 'trailguide.js',
+    library: 'TrailGuide',
+    libraryTarget: 'assign'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  externals: {
+    'react': 'commonjs react' 
+  }
+}
