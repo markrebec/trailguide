379fb438f32591610ee30b28c06ed29c484de135
Mark Rebec <mark@markrebec.com>
2019-03-06 18:26:56 -0800

    few more small ui improvements

--- spec/dummy/config/experiments.rb
+++ spec/dummy/config/experiments.rb
@@ -7,5 +7,6 @@ experiment :combined_experiment do |config|
   variant :option_a
   variant :option_b
   variant :option_c
+  config.goals = [:some_goal, :other_goal]
   config.combined = [:first_combo, :last_combo]
 end
