9eeca376f797238417dde95c5ed5c409768c52cf
Mark Rebec <mark@markrebec.com>
2019-03-06 18:49:57 -0800

    more ui improvements

--- app/controllers/trail_guide/admin/experiments_controller.rb
+++ app/controllers/trail_guide/admin/experiments_controller.rb
@@ -10,32 +10,32 @@ module TrailGuide
 
       def start
         experiment.start!
-        redirect_to :back rescue redirect_to trail_guide_admin.experiments_path
+        redirect_to trail_guide_admin.experiments_path(anchor: experiment.experiment_name)
       end
 
       def stop
         experiment.stop!
-        redirect_to :back rescue redirect_to trail_guide_admin.experiments_path
+        redirect_to trail_guide_admin.experiments_path(anchor: experiment.experiment_name)
       end
 
       def reset
         experiment.reset!
-        redirect_to :back rescue redirect_to trail_guide_admin.experiments_path
+        redirect_to trail_guide_admin.experiments_path(anchor: experiment.experiment_name)
       end
 
       def resume
         experiment.resume!
-        redirect_to :back rescue redirect_to trail_guide_admin.experiments_path
+        redirect_to trail_guide_admin.experiments_path(anchor: experiment.experiment_name)
       end
 
       def restart
         experiment.reset! && experiment.start!
-        redirect_to :back rescue redirect_to trail_guide_admin.experiments_path
+        redirect_to trail_guide_admin.experiments_path(anchor: experiment.experiment_name)
       end
 
       def winner
         experiment.declare_winner!(params[:variant])
-        redirect_to :back rescue redirect_to trail_guide_admin.experiments_path
+        redirect_to trail_guide_admin.experiments_path(anchor: experiment.experiment_name)
       end
 
       private
