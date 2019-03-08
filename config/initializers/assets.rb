6c3326f74e113c117310a513b17a82bc5ac1d483
Mark Rebec <mark@markrebec.com>
2019-03-07 19:47:11 -0800

    introduces admin config and does some more ui work

---
+++ config/initializers/assets.rb
@@ -0,0 +1,1 @@
+Rails.application.config.assets.precompile += %w( trail_guide/trailguide.png )
--- lib/trail_guide/admin.rb
+++ lib/trail_guide/admin.rb
@@ -2,5 +2,11 @@ require "trail_guide/admin/engine"
 
 module TrailGuide
   module Admin
+    include Canfig::Module
+
+    configure do |config|
+      config.title = 'TrailGuide'
+      config.subtitle = 'Experiments and A/B Tests'
+    end
   end
 end
