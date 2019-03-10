eb0a90cab45d99aae09147422de32b7a84b17d95
Mark Rebec <mark@markrebec.com>
2019-03-09 18:37:10 -0800

    uses strict config for core engine config

--- lib/trail_guide/config.rb
+++ lib/trail_guide/config.rb
@@ -1,5 +1,15 @@
 module TrailGuide
-  class Config < Canfig::OpenConfig
+  class Config < Canfig::Config
+    DEFAULT_KEYS = [
+      :redis, :disabled, :override_parameter, :allow_multiple_experiments,
+      :adapter, :on_adapter_failover, :filtered_ip_addresses,
+      :filtered_user_agents, :request_filter
+    ].freeze
+
+    def initialize(*args, **opts, &block)
+      args = args.concat(DEFAULT_KEYS)
+      super(*args, **opts, &block)
+    end
 
     def configure(*args, &block)
       super(*args) do |config|
