188b168a5e59472a18527233d799d4709d68b1b5
Mark Rebec <mark@markrebec.com>
2019-05-04 18:05:52 -0700

    remove deprecate dummy experiment

--- spec/dummy/app/controllers/homepage_controller.rb
+++ spec/dummy/app/controllers/homepage_controller.rb
@@ -1,21 +1,4 @@
 class HomepageController < ApplicationController
   def index
-    trailguide.choose!(:weed_test) do |variant|
-      ap variant.to_s
-    end
-    trailguide.run!(:weed_test)
-    #trailguide.render!(:weed_test)
-  end
-
-  def sativa
-    ap "SATIVA"
-  end
-
-  def indica
-    ap "INDICA"
-  end
-
-  def hybrid
-    ap "HYBRID"
   end
 end
