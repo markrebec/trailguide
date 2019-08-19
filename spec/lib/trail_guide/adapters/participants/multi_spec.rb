0feff1b4306ec5598d8b702566b06e55cc0d6411
Mark Rebec <mark@markrebec.com>
2019-08-19 10:36:00 -0700

    more coverage for remaining adapters

--- spec/lib/trail_guide/adapters/participants/multi_spec.rb
+++ spec/lib/trail_guide/adapters/participants/multi_spec.rb
@@ -1,5 +1,94 @@
 require 'rails_helper'
+require 'shared_examples/participant_adapter'
 
 RSpec.describe TrailGuide::Adapters::Participants::Multi do
-  pending
+  subject { described_class.new(context) }
+  let(:context) { Class.new { }.new }
+
+  describe '#initialize' do
+    context 'with a custom adapter proc' do
+      let(:context) {
+        Class.new {
+          def trailguide_user
+            Struct.new(:id).new(1)
+          end
+        }.new
+      }
+      let(:adapter) { -> (ctx) { return TrailGuide::Adapters::Participants::Anonymous } }
+      subject { described_class.new(context) { |cfg| cfg.adapter = adapter } }
+
+      it 'calls the configured proc' do
+        expect(adapter).to receive(:call).and_return(TrailGuide::Adapters::Participants::Anonymous)
+        described_class.new(context) { |cfg| cfg.adapter = adapter }
+      end
+
+      it 'uses the returned adapter' do
+        expect(subject).to be_an_instance_of(TrailGuide::Adapters::Participants::Anonymous::Adapter)
+      end
+    end
+
+    context 'with the default adapter proc' do
+      context 'when the context responds with a trailguide_user' do
+        let(:context) {
+          Class.new {
+            def trailguide_user
+              Struct.new(:id).new(1)
+            end
+          }.new
+        }
+
+        it 'uses the redis adapter' do
+          expect(subject).to be_an_instance_of(TrailGuide::Adapters::Participants::Redis::Adapter)
+        end
+      end
+
+      context 'when the context responds with a current_user' do
+        let(:context) {
+          Class.new {
+            def current_user
+              Struct.new(:id).new(1)
+            end
+          }.new
+        }
+
+        it 'uses the redis adapter' do
+          expect(subject).to be_an_instance_of(TrailGuide::Adapters::Participants::Redis::Adapter)
+        end
+      end
+
+      context 'when the context supports cookies' do
+        let(:context) {
+          Class.new {
+            def cookies
+              @cookies ||= {}
+            end
+          }.new
+        }
+
+        it 'uses the cookie adapter' do
+          expect(subject).to be_an_instance_of(TrailGuide::Adapters::Participants::Cookie::Adapter)
+        end
+      end
+
+      context 'when the context supports sessions' do
+        let(:context) {
+          Class.new {
+            def session
+              @session ||= {}
+            end
+          }.new
+        }
+
+        it 'uses the session adapter' do
+          expect(subject).to be_an_instance_of(TrailGuide::Adapters::Participants::Session::Adapter)
+        end
+      end
+
+      context 'when the context does not support cookies, sessions or identifying a user' do
+        it 'uses the anonymous adapter' do
+          expect(subject).to be_an_instance_of(TrailGuide::Adapters::Participants::Anonymous::Adapter)
+        end
+      end
+    end
+  end
 end
--- spec/lib/trail_guide/adapters/participants/redis_spec.rb
+++ spec/lib/trail_guide/adapters/participants/redis_spec.rb
@@ -1,5 +1,75 @@
 require 'rails_helper'
+require 'shared_examples/participant_adapter'
 
 RSpec.describe TrailGuide::Adapters::Participants::Redis do
-  pending
+  subject { described_class.new(context) }
+  let(:context) do
+    Class.new { }.new
+  end
+
+  it_behaves_like 'a participant adapter'
+
+  describe '#initialize' do
+    it 'returns an Adapter class' do
+      expect(subject).to be_an_instance_of(TrailGuide::Adapters::Participants::Redis::Adapter)
+    end
+
+    it 'sets a default key namespace' do
+      expect(subject.config.namespace).to eq(:participants)
+    end
+
+    context 'when configured with a key namespace' do
+      subject { described_class.new(context) { |cfg| cfg.namespace = :foobar } }
+
+      it 'uses the configured key namespace' do
+        expect(subject.config.namespace).to eq(:foobar)
+      end
+    end
+
+    context 'when configured with a lookup proc' do
+      let(:lookup) { -> (ctx) { return :foobar } }
+      subject { described_class.new(context) { |cfg| cfg.lookup = lookup } }
+
+      it 'uses the configured lookup proc' do
+        expect(subject.config.lookup).to respond_to(:call)
+      end
+
+      it 'uses the return value to set the storage key' do
+        expect(subject.instance_variable_get(:@storage_key)).to eq("#{subject.config.namespace}:foobar")
+      end
+    end
+
+    context 'when configured with a lookup method' do
+      subject { described_class.new(context) { |cfg| cfg.lookup = :lookup_method } }
+      let(:context) do
+        Class.new do
+          def lookup_method
+            :foobar
+          end
+        end.new
+      end
+
+      it 'uses the configured lookup method' do
+        expect(subject.config.lookup).to eq(:lookup_method)
+      end
+
+      it 'uses the return value to set the storage key' do
+        expect(subject.instance_variable_get(:@storage_key)).to eq("#{subject.config.namespace}:foobar")
+      end
+    end
+
+    context 'when a key argument is provided' do
+      subject { described_class::Adapter.new(context, described_class.new(context).configuration, key: :foobar) }
+
+      it 'uses the key instead of the lookup proc' do
+        expect(subject.instance_variable_get(:@storage_key)).to eq("#{subject.config.namespace}:foobar")
+      end
+    end
+
+    context 'when no lookup proc is configured' do
+      it 'raises an ArgumentError' do
+        expect { described_class.new(nil) { |cfg| cfg.lookup = nil } }.to raise_exception(ArgumentError)
+      end
+    end
+  end
 end
