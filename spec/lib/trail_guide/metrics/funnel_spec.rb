af1e82e277117b2ff42c004132f526a10d7f0246
Mark Rebec <mark@markrebec.com>
2019-08-19 12:25:14 -0700

    placeholder specs for funnels and checkpoints

--- spec/lib/trail_guide/metrics/funnel_spec.rb
+++ spec/lib/trail_guide/metrics/funnel_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe TrailGuide::Metrics::Funnel do
-  pending
+  experiment
+  let(:name) { :foo }
+  subject { described_class.new(experiment, name) }
+
+  describe '#initialize' do
+    it 'sets the @experiment' do
+      expect(subject.instance_variable_get(:@experiment)).to eq(experiment)
+    end
+
+    it 'sets the @name' do
+      expect(subject.instance_variable_get(:@name)).to eq(name)
+    end
+  end
+
+  pending 'TODO finish work on implementing funnels'
 end
