dd3d7a11e652f011992472e986e5fef2bb5527d3
Mark Rebec <mark@markrebec.com>
2019-08-20 11:18:40 -0700

    handles errors and warnings for static algo

--- lib/trail_guide/algorithms/static.rb
+++ lib/trail_guide/algorithms/static.rb
@@ -11,15 +11,20 @@ module TrailGuide
       end
 
       def new(experiment)
+        TrailGuide.logger.warn "WARNING: Using the Static algorithm for an experiment which is configured with sticky_assignment. You should either use a different algorithm or configure sticky_assignment for the `#{experiment.experiment_name}` experiment." if experiment.configuration.sticky_assignment?
         self.class.new(experiment, &@block)
       end
 
-      def choose!(metadata:)
+      def choose!(metadata: nil)
         return experiment.control unless metadata.present?
 
         experiment.variants.find do |variant|
           @block.call(variant.metadata, metadata)
         end || experiment.control
+      rescue => e
+        TrailGuide.logger.error "#{e.class.name}: #{e.message}"
+        TrailGuide.logger.error e.backtrace.first
+        experiment.control
       end
     end
   end
