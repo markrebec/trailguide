f3f6944232a63ddea9006bc579939ce314cb786e
Mark Rebec <mark@markrebec.com>
2020-04-08 14:25:26 -0700

    uses configured override parameter properly when generating preview urls from admin

--- app/controllers/trail_guide/admin/application_controller.rb
+++ app/controllers/trail_guide/admin/application_controller.rb
@@ -5,13 +5,14 @@ module TrailGuide
 
       def preview_url(variant, *args, **opts)
         config_url = variant.experiment.configuration.preview_url
-        opts = opts.merge({experiment: {variant.experiment.experiment_name => variant.name}})
+        param = TrailGuide.configuration.override_parameter
+        opts = opts.merge({param => {variant.experiment.experiment_name => variant.name}})
         if config_url.respond_to?(:call)
           main_app.instance_exec *args, **opts, &config_url
         elsif config_url.is_a?(Symbol)
           main_app.send(config_url, *args, **opts)
         else
-          config_url.to_s + "?experiment[#{variant.experiment.experiment_name}]=#{variant.name}"
+          config_url.to_s + "?#{param.to_s}[#{variant.experiment.experiment_name}]=#{variant.name}"
         end
       end
       helper_method :preview_url
