44b9d2854235efb192da11cbffb1713daf1564e6
Mark Rebec <mark@markrebec.com>
2020-04-11 10:54:22 -0700

    keeps axios default transforms for consistent behavior without needing to think about it

--- src/index.js
+++ src/index.js
@@ -4,7 +4,20 @@ export const client = (axiosConfig) => {
   if (typeof axiosConfig == "string")
     axiosConfig = { baseURL: axiosConfig }
 
+  if (axiosConfig.transformRequest)
+    axiosConfig.transformRequest = [
+      ...axiosConfig.transformRequest,
+      ...axios.defaults.transformRequest
+    ]
+
+  if (axiosConfig.transformResponse)
+    axiosConfig.transformResponse = [
+      ...axios.defaults.transformResponse,
+      ...axiosConfig.transformResponse
+    ]
+
   const axiosClient = axios.create(axiosConfig)
+
   return {
     active: (requestConfig=undefined) =>
       axiosClient.get('/', requestConfig),
