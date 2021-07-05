d538ef8376b339cef9ebb51e0d696165f272841c
Mark Rebec <mark@markrebec.com>
2021-07-05 13:51:55 -0700

    configures lcov for github workflow action

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,11 @@
 require 'simplecov'
 require 'simplecov-lcov'
+SimpleCov::Formatter::LcovFormatter.config do |c|
+  c.report_with_single_file = true
+  c.output_directory = 'coverage/'
+  c.lcov_file_name = 'lcov.info'
+  # c.single_report_path = 'coverage/lcov.info'
+end
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::HTMLFormatter,
   SimpleCov::Formatter::LcovFormatter
