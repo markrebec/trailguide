345404aa807e937201d44d2fd74d03d929a99eb4
Mark Rebec <mark@markrebec.com>
2021-07-14 13:48:02 -0700

    refactors enrollment a bit to organize the logical steps when choosing a variant

--- lib/trail_guide/experiments/enrollment.rb
+++ lib/trail_guide/experiments/enrollment.rb
@@ -19,59 +19,16 @@ module TrailGuide
 
       def choose_variant!(override: nil, excluded: false, metadata: nil)
         return control if TrailGuide.configuration.disabled
-
-        if override.present?
-          variant = variants.find { |var| var == override } || control
-          if running? && !is_combined?
-            variant.increment_participation! if configuration.track_override
-            participant.participating!(variant) if configuration.store_override
-          end
-          return variant
-        end
-
-        if winner?
-          variant = winning_variant
-          if track_winner_conversions? && running?
-            variant.increment_participation! unless participant.variant == variant || !track_participation?(metadata)
-            participant.exit! if participant.participating? && participant.variant != variant
-            participant.participating!(variant)
-          end
-          return variant
-        end
-
+        return choose_override!(override) if override.present?
+        return choose_winner!(metadata) if winner?
         return control if excluded || stopped?
-
-        if !started? && start_manually?
-          if enable_calibration?
-            unless participant.variant == control
-              control.increment_participation!
-              parent.control.increment_participation! if is_combined?
-            end
-
-            if participant.participating? && participant.variant != control
-              participant.exit!
-              parent.participant.exit! if is_combined?
-            end
-
-            participant.participating!(control)
-            parent.participant.participating!(parent.control) if is_combined?
-          end
-          return control
-        end
+        return choose_calibrating! if !started? && start_manually?
 
         start! unless started? || scheduled?
         return control unless running?
 
-        # only re-use the variant for experiments that store participation,
-        # all other (i.e. content-based) experiments should re-select and
-        # re-assign on enrollment
-        if configuration.sticky_assignment? && participant.participating?
-          variant = participant.variant
-          participant.participating!(variant) if track_participation?(metadata)
-          return variant
-        end
+        return choose_sticky!(metadata) if configuration.sticky_assignment? && participant.participating?
 
-        return control unless is_combined? || TrailGuide.configuration.allow_multiple_experiments == true || !participant.participating_in_active_experiments?(TrailGuide.configuration.allow_multiple_experiments == false)
         return control unless allow_participation?(metadata)
 
         variant = algorithm_choose!(metadata: metadata)
@@ -83,13 +40,61 @@ module TrailGuide
         variant
       end
 
+      def choose_override!(override)
+        variant = variants.find { |var| var == override } || control
+        if running? && !is_combined?
+          variant.increment_participation! if configuration.track_override
+          participant.participating!(variant) if configuration.store_override
+        end
+        return variant
+      end
+
+      def choose_winner!(metadata)
+        variant = winning_variant
+        if track_winner_conversions? && running?
+          variant.increment_participation! unless participant.variant == variant || !track_participation?(metadata)
+          participant.exit! if participant.participating? && participant.variant != variant
+          participant.participating!(variant)
+        end
+        return variant
+      end
+
+      def choose_calibrating!
+        if enable_calibration?
+          unless participant.variant == control
+            control.increment_participation!
+            parent.control.increment_participation! if is_combined?
+          end
+
+          if participant.participating? && participant.variant != control
+            participant.exit!
+            parent.participant.exit! if is_combined?
+          end
+
+          participant.participating!(control)
+          parent.participant.participating!(parent.control) if is_combined?
+        end
+        return control
+      end
+
+      def choose_sticky!(metadata)
+        variant = participant.variant
+        participant.participating!(variant) if track_participation?(metadata)
+        return variant
+      end
+
       def algorithm_choose!(metadata: nil)
         algorithm.choose!(metadata: metadata)
       end
 
+      def participant_allowed?
+        is_combined? || TrailGuide.configuration.allow_multiple_experiments == true ||
+          !participant.participating_in_active_experiments?(TrailGuide.configuration.allow_multiple_experiments == false)
+      end
+
       def allow_participation?(metadata=nil)
-        return true if callbacks[:allow_participation].empty?
-        run_callbacks(:allow_participation, true, participant, metadata)
+        return participant_allowed? if callbacks[:allow_participation].empty?
+        return run_callbacks(:allow_participation, true, participant, metadata) if participant_allowed?
       end
 
       def track_participation?(metadata=nil)
